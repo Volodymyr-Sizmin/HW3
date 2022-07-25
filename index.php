59c09817f69f2f5abb49727a6126f0865509f630
Volodymyr Sizmin <ukrmedias5@gmail.com>
2022-07-25 23:10:02 +0300

    add homework colors

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+class Colors
+{
+
+    private static function setRed($red)
+    {
+        if ($red < 0 or $red > 255) {
+            throw new Exception("Only numbers from 0 to 255");
+        }
+    }
+
+    private static function setGreen($green)
+    {
+        if ($green < 0 or $green > 255) {
+            throw new Exception("Only numbers from 0 to 255");
+        }
+    }
+
+    private static function setBlue($blue)
+    {
+        if ($blue < 0 or $blue > 255) {
+            throw new Exception("Only numbers from 0 to 255");
+        }
+    }
+
+    public function __construct(
+        private int $red,
+        private int $green,
+        private int $blue)
+    {
+        self::setRed($red);
+
+        self::setGreen($green);
+
+        self::setBlue($blue);
+
+    }
+
+    public function getRed()
+    {
+        return $this->red;
+    }
+
+    public function getGreen()
+    {
+        return $this->green;
+    }
+
+    public function getBlue()
+    {
+        return $this->blue;
+    }
+
+    public static function equals($obj1, $obj2)
+    {
+        if ($obj1 == $obj2) {
+            return true;
+        } else return false;
+    }
+
+    public static function random()
+    {
+        $rand1 = new Colors(rand(0, 255), rand(0, 255), rand(0, 255));
+        return $rand1;
+    }
+   public function mix(self $color)
+    {
+        $value = ($color->getRed() + $color->getBlue() + $color->getGreen()) / 2;
+   return new Colors($value, $value, $value);
+}
+ }
+try {
+    $color1 = new Colors(100,100,100);
+    $color2 = new Colors(100, 100, 100);
+    } catch (Exception $e) {
+    echo $e->getmessage();
+}
+
+var_dump(Colors::equals($color1, $color2));
+echo "<br>";
+$color3 = Colors::random();
+echo "<br>";
+echo $color3->getRed()."<br>";
+echo $color3->getGreen()."<br>";
+echo $color3->getBlue()."<br>";
+$color4 = new Colors(200, 200, 200);
+$mixedColor = $color4->mix(new Colors(100, 100, 100));
+echo $mixedColor->getRed()."<br>";
+echo $mixedColor->getGreen()."<br>";
+echo $mixedColor->getBlue()."<br>";
+
+
